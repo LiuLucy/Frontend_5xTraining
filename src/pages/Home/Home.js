86de09b20c8e3ae9d34cf978246fa9d7c64171c2
Lucy <lucy933626@gmail.com>
2020-02-05 21:07:33 +0800

    about block done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -156,6 +156,7 @@ function Home() {
       </div>
       {/* fp-carousel */}
 
+      {/* about */}
       <div className="about-5x">
         <div className="title text-center mt-5">
           <h4>關於五倍紅寶石 About 5xRuby</h4>
@@ -165,24 +166,58 @@ function Home() {
         </div>
         <div className="container mt-5">
           <div className="row justify-content-center">
-            <div className="col-12 col-md-3 justify-content-center d-flex">
-              <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img1-0d20ab19.png"></img>
+            <div className="col-12 col-md-3">
+              <div className="group-vertical text-center">                
+                <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img1-0d20ab19.png"></img>
+                <div className="mt-3">
+                  <h4 className="mb-0">網頁設計</h4>        
+                  <h4 className="mb-0">前後端課程教學</h4>
+                </div>
+                <div className="mt-3">
+                  <p>帶你掌握程式技能轉職工程師，程式課程從入門到進階讓你快速上手，學員好評推薦轉職課程！</p>
+                </div>
+              </div>
             </div>
-            <div className="col-12 col-md-3 justify-content-center d-flex">
-              <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img2-26e4ca80.png"></img>
-            
+            <div className="col-12 col-md-3">
+              <div className="group-vertical text-center">
+                <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img2-26e4ca80.png"></img>
+                <div className="mt-3">
+                  <h4 className="mb-0">頂尖技術</h4>
+                  <h4 className="mb-0">網頁製作、專案開發</h4>
+                </div>
+                <div className="mt-3">
+                  <p>網站開發到行動裝置 App 工程服務、系統設計開發甚至系統架設，提供專業的建議與頂尖的技術。</p>
+                </div>
+              </div>
             </div>
-            <div className="col-12 col-md-3 justify-content-center d-flex">
-              <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img3-d0a4089e.png"></img>
-            
+            <div className="col-12 col-md-3">
+              <div className="group-vertical text-center">
+                <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img3-d0a4089e.png"></img>
+                <div className="mt-3">
+                  <h4 className="mb-0">資訊軟體開發</h4>
+                  <h4 className="mb-0">技術顧問</h4>
+                </div>
+                <div className="mt-3">
+                  <p>協助您將現有人力快速打造為 Ruby/Rails 團隊，無須經歷繁瑣的人才招募就有即戰力！</p>
+                </div>
+              </div>
             </div>
-            <div className="col-12 col-md-3 justify-content-center d-flex">
-              <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img4-13321bf0.png"></img>
-            
+            <div className="col-12 col-md-3">
+              <div className="group-vertical text-center">
+                <img src="https://5xruby.tw/assets/images/index/feature-list/feature-list-img4-13321bf0.png"></img>
+                <div className="mt-3">
+                  <h4 className="mb-0">前後端工程師</h4>
+                  <h4 className="mb-0">社群經營</h4>
+                </div>
+                <div className="mt-3">
+                  <p>Ruby 社群經營、舉辦各類活動，促進 Ruby 社群發展，歡迎前後端工程師分享交流。</p>
+                </div>
+              </div>
             </div>        
           </div>  
         </div>  
       </div>
+      {/* about */}      
     </div>
   );
 }
