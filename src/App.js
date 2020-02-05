9d38ce3acae1425b65bd3b2657a0be8cb2a207cd
Lucy <lucy933626@gmail.com>
2020-02-05 10:03:28 +0800

    style path fix

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './assets/img/logo.png';
 import pressImg from './assets/img/press-img.png';
 import cakeresume from './assets/img/cakeresume.png';
 
-import './App.css';
+import './style/Style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header px-3">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#"><img src={logo} /></a>
+        <a className="navbar-brand" href="https://5xruby.tw/"><img src={logo} alt="圖片" /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -18,25 +18,25 @@ function App() {
           
         <ul class="navbar-nav">
           <li class="nav-item mx-2">
-            <a class="nav-link d-flex justify-content-center" href="#"><i className="far fa-gem mr-2 d-flex align-items-center"></i>線上課程</a>
+            <a class="nav-link d-flex justify-content-center" href="https://5xruby.tw/"><i className="far fa-gem mr-2 d-flex align-items-center"></i>線上課程</a>
           </li>          
           <li class="nav-item mx-2">
-            <a class="nav-link d-flex justify-content-center" href="#"><i className="far fa-compass fa-lg mr-2 d-flex align-items-center"></i>ASTRO Camp<span className="tag p-1 ml-2">報名優惠中</span></a>
+            <a class="nav-link d-flex justify-content-center" href="https://5xruby.tw/"><i className="far fa-compass fa-lg mr-2 d-flex align-items-center"></i>ASTRO Camp<span className="tag p-1 ml-2">報名優惠中</span></a>
           </li>
           <li class="nav-item mx-2">
-            <a class="nav-link d-flex justify-content-center" href="#">短期課程</a>
+            <a class="nav-link d-flex justify-content-center" href="https://5xruby.tw/">短期課程</a>
           </li> 
           <li class="nav-item mx-2">
-            <a class="nav-link d-flex justify-content-center" href="#">專案開發</a>
+            <a class="nav-link d-flex justify-content-center" href="https://5xruby.tw/">專案開發</a>
           </li> 
           <li class="nav-item mx-2">
-            <a class="nav-link d-flex justify-content-center" href="#">企業代訓</a>
+            <a class="nav-link d-flex justify-content-center" href="https://5xruby.tw/">企業代訓</a>
           </li> 
           <li class="nav-item mx-2">
-            <a class="nav-link d-flex justify-content-center" href="#">空間租借</a>
+            <a class="nav-link d-flex justify-content-center" href="https://5xruby.tw/">空間租借</a>
           </li> 
           <li class="nav-item mx-2">
-            <a class="nav-link d-flex justify-content-center" href="#">最新消息</a>
+            <a class="nav-link d-flex justify-content-center" href="https://5xruby.tw/">最新消息</a>
           </li> 
         </ul>
         </div>
@@ -49,11 +49,11 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col-12 col-md-3">
-              <a className="press-img ">
-                <img width="100%" className="mb-3" src={pressImg}/>
+              <a className="press-img" href="https://5xruby.tw/">
+                <img width="100%" className="mb-3" src={pressImg} alt="圖片"/>
               </a>
-              <a className="press-img">
-                <img className="mr-2" src={cakeresume} width="70%"/>
+              <a className="press-img" href="https://5xruby.tw/">
+                <img className="mr-2" src={cakeresume} width="70%" alt="圖片" />
                 <span >找工作</span>
               </a>
             </div>
@@ -61,15 +61,15 @@ function App() {
               <div className="row">
                 <div className="col-12 mt-1 mt-md-0">
                   <div className="footer-nav">
-                    <h5><a>關於五倍紅寶石</a></h5>
-                    <h5><a>媒體報導</a></h5>
-                    <h5><a>團隊成員</a></h5>
-                    <h5><a>聯絡詢價</a></h5>
-                    <h5><a>常見問題</a></h5>
-                    <h5><a>工作機會</a></h5>
-                    <h5><a>開源專案</a></h5>
-                    <h5><a>隱私條款</a></h5>
-                    <h5><a>服務條款</a></h5>
+                    <h5><a href="https://5xruby.tw/">關於五倍紅寶石</a></h5>
+                    <h5><a href="https://5xruby.tw/">媒體報導</a></h5>
+                    <h5><a href="https://5xruby.tw/">團隊成員</a></h5>
+                    <h5><a href="https://5xruby.tw/">聯絡詢價</a></h5>
+                    <h5><a href="https://5xruby.tw/">常見問題</a></h5>
+                    <h5><a href="https://5xruby.tw/">工作機會</a></h5>
+                    <h5><a href="https://5xruby.tw/">開源專案</a></h5>
+                    <h5><a href="https://5xruby.tw/">隱私條款</a></h5>
+                    <h5><a href="https://5xruby.tw/">服務條款</a></h5>
                   </div>
                 </div>
                 <div className="col-12 mt-4">
@@ -77,9 +77,9 @@ function App() {
                       <div className="col-12 col-md-4 col-xl-3">
                         <strong className="phone-number">02-2331-5247</strong><br />
                         <small>Mon - Fri / 09:30 - 22:00</small><br />
-                        <a><i className="mr-3 ml-2 mt-3 media-icon fab fa-facebook-f"></i></a>
-                        <a><i className="mx-2 mt-3 media-icon fab fa-twitter"></i></a>
-                        <a><i className="mx-2 mt-3 media-icon fab fa-github"></i></a>
+                        <a href="https://5xruby.tw/"><i className="mr-3 ml-2 mt-3 media-icon fab fa-facebook-f"></i></a>
+                        <a href="https://5xruby.tw/"><i className="mx-2 mt-3 media-icon fab fa-twitter"></i></a>
+                        <a href="https://5xruby.tw/"><i className="mx-2 mt-3 media-icon fab fa-github"></i></a>
                       </div>
                       <div className="col-12 col-md-8 col-xl-9 mt-3 mt-md-0">
                         <p>有任何問題？</p>
